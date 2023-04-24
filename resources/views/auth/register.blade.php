6a6e30564b081d6e08513cd2f1e2f5a759e7d0e0
sooto selim <adewaleselim6@gmail.com>
2023-04-24 10:33:37 +0100

    leaning laravel mass assignmet

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,10 +1,10 @@
 <x-guest-layout>
     <form method="POST" action="{{ route('register') }}">
         @csrf
-fjjf
+
         <!-- Name -->
         <div>
-            <x-input-label for="name" :value="__('Name')" />
+        <x-input-label for="name" :value="__('Name')" />
             <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
